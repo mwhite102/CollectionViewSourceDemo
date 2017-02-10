566b723b128e8da51a30744bba5b9d81335498f6
mwhite102 <mwhite102@gmail.com>
2017-02-10 08:51:37 -0600

    Initial Application Layout

--- CollectionViewSourceDemo/MainWindow.xaml.cs
+++ CollectionViewSourceDemo/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CollectionViewSourceDemo.Models;
+using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CollectionViewSourceDemo
 {
@@ -23,6 +12,22 @@ namespace CollectionViewSourceDemo
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+
+            Init();
+        }
+
+        public ObservableCollection<PersonModel> People { get; } = new ObservableCollection<PersonModel>();
+
+        private void Init()
+        {
+            People.Add(new PersonModel() { Name = "Mary Ann", Age = 24 });
+            People.Add(new PersonModel() { Name = "Ginger", Age = 28 });
+        }
+
+        private void Add_Button_Click(object sender, RoutedEventArgs e)
+        {
+            People.Add(new PersonModel() { Name = Name_TextBox.Text, Age = int.Parse(Age_TextBox.Text) });
         }
     }
 }
